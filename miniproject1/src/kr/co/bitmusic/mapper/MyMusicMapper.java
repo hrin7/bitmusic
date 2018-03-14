6e391a02fd0b439be167f67ec210363f8a8c3228
선영 <100479sy@daum.net>
2018-03-14 19:30:47 +0900

    Mymusic과 관련된 내용 - 미완성입니다..ㅜ

--- miniproject1/src/kr/co/bitmusic/mapper/MyMusicMapper.java
+++ miniproject1/src/kr/co/bitmusic/mapper/MyMusicMapper.java
@@ -1,5 +1,14 @@
+// 선영
 package kr.co.bitmusic.mapper;
+
+import java.util.List;
+
+import kr.co.bitmusic.domain.Music;
 
 public interface MyMusicMapper {
 
+	public List<Music> selectMyMusicAll ();
+	
+	
+	
 }
