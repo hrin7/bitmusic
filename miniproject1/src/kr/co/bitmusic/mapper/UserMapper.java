5f48f40609778adf09b01bcfa767dc1a7b61d96e
Sujin <sujinki6020@gmail.com>
2018-03-15 15:03:28 +0900

    updateUser관련 올립니다~

--- miniproject1/src/kr/co/bitmusic/mapper/UserMapper.java
+++ miniproject1/src/kr/co/bitmusic/mapper/UserMapper.java
@@ -19,4 +19,5 @@ public interface UserMapper {
 	int deleteUser (String id);
 	
 	User loginUser(User user);
+	
 }
--- miniproject1/src/kr/co/bitmusic/ui/UpdateUserUI.java
+++ miniproject1/src/kr/co/bitmusic/ui/UpdateUserUI.java
@@ -1,37 +1,53 @@
 package kr.co.bitmusic.ui;
 
+import kr.co.bitmusic.common.Session;
 import kr.co.bitmusic.domain.User;
 import kr.co.bitmusic.mapper.UserMapper;
 
 public class UpdateUserUI extends BaseBitMusicUI {
 	
+	private User saved = Session.getUser();
 	private UserMapper userMapper;
 	public UpdateUserUI(UserMapper userMapper) {
 		this.userMapper = userMapper;
 	}
 	
 	public void service() {
-			BaseBitMusicUI ui = null;
-			BitMusicUI bmu = null;
+		 User user = new User();
 			while (true) {
 				switch (menu()) {
-				case 1: UpdateUserUI(SearchType = id); break;
-				case 2: ; break;
-				case 3: ; break;
-				case 4: ; break;
-				case 0: bmu.service(); break;
+				case 1:
+					user.setId(saved.getId());
+					user.setPassword(getStr("변경할 비밀번호를 입력하세요 : "));
+					userMapper.updateUserPassword(user);
+					break;
+				case 2: 
+					user.setId(saved.getId());
+					user.setPasswordHint(getStr("변경할 비밀번호 힌트를 입력하세요 : "));
+					userMapper.updateUserPasswordHint(user);
+					break;
+				case 3: 
+					user.setId(saved.getId());
+					user.setEmail(getStr("변경할 이메일을 입력하세요 : "));
+					userMapper.updateUserEmail(user);
+					break;
+				case 0: returnToMain(); break;
 				}
-				ui.service();
+				System.out.println("회원정보가 수정되었습니다");
 			}
 	}
 	
 	private int menu() {
-		System.out.println("1. Id 변경");
-		System.out.println("2. Password 변경");
-		System.out.println("3. Password Hint 변경");
-		System.out.println("4. 이메일 변경");
-		System.out.println("0. main메뉴로 돌아가기");
+		System.out.println("1. 비밀번호 변경");
+		System.out.println("2. 비밀번호 힌트 변경");
+		System.out.println("3. 이메일 변경");
+		System.out.println("0. 메인메뉴로 돌아가기");
 		return getInt("수정할 메뉴번호를 입력하세요 : ");
+	}
+	
+	public void returnToMain() {
+		BitMusicUI ui = new BitMusicUI();
+		ui.service();
 	}
 
 }
