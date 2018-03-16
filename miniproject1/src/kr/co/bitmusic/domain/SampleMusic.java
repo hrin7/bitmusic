f787486feb92de01134bfa95bdbe534f964e5b55
Sujin <sujinki6020@gmail.com>
2018-03-16 11:39:43 +0900

    sample music VO, xml, mapper입니다!

---
+++ miniproject1/src/kr/co/bitmusic/domain/SampleMusic.java
@@ -0,0 +1,81 @@
+package kr.co.bitmusic.domain;
+
+public class SampleMusic {
+	
+	private int no; 
+	
+	private String title;
+	
+	private String singer;
+	
+	private String genre;
+	
+	private String lyrics;
+	
+	private String relDate;
+	
+	private String sampleMusicPath;
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSinger() {
+		return singer;
+	}
+
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getLyrics() {
+		return lyrics;
+	}
+
+	public void setLyrics(String lyrics) {
+		this.lyrics = lyrics;
+	}
+
+	public String getRelDate() {
+		return relDate;
+	}
+
+	public void setRelDate(String relDate) {
+		this.relDate = relDate;
+	}
+
+	public String getSampleMusicPath() {
+		return sampleMusicPath;
+	}
+
+	public void setSampleMusicPath(String sampleMusicPath) {
+		this.sampleMusicPath = sampleMusicPath;
+	}
+
+	@Override
+	public String toString() {
+		return "SampleMusic [no=" + no + ", title=" + title + ", singer=" + singer + ", genre=" + genre + ", lyrics="
+				+ lyrics + ", relDate=" + relDate + ", sampleMusicPath=" + sampleMusicPath + "]";
+	}
+
+}
