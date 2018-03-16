24a0bdaa8e3090e1118b1e9b62367211d224fc85
선영 <100479sy@daum.net>
2018-03-16 18:10:32 +0900

    메인이랑 가사파일

--- miniproject1/src/kr/co/bitmusic/main/MusicPlay.java
+++ miniproject1/src/kr/co/bitmusic/main/MusicPlay.java
@@ -1,10 +1,28 @@
 package kr.co.bitmusic.main;
 
+import java.io.FileInputStream;
+
+import javazoom.jl.player.Player;
 import kr.co.bitmusic.ui.BitMusicUI;
 
 public class MusicPlay {
 	public static void main(String[] args) {
+		
 		BitMusicUI ui = new BitMusicUI();
-		ui.service();
+		try {
+			Player p = new Player(new FileInputStream("sound/welcom.mp3"));
+			System.out.println("♬  비트 뮤직에 오신걸 환영합니다 ♬");
+			System.out.println();
+			p.play();
+			p.close();
+			
+			ui.service();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 }
