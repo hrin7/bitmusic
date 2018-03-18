d7c86124dfaf477c474f048ea0638c5815327c2a
선영 <100479sy@daum.net>
2018-03-18 20:09:17 +0900

    회원탈퇴 조건 수정!

--- miniproject1/src/kr/co/bitmusic/ui/DeleteUserUI.java
+++ miniproject1/src/kr/co/bitmusic/ui/DeleteUserUI.java
@@ -5,18 +5,19 @@ import kr.co.bitmusic.domain.User;
 import kr.co.bitmusic.mapper.UserMapper;
 
 public class DeleteUserUI extends BaseBitMusicUI {
-	
-	User saved = Session.getUser();
+	private User saved = Session.getUser();
 	
 	public void service() {
 		User user = new User();
-		user.setPassword(getStr("정말 탈퇴하시겠습니까? 비밀번호를 한 번 더 입력해 주세요 : "));
-		if(user.getPassword()==saved.getPassword()) {
+		int result = (getInt("정말 탈퇴하시겠습니까? 1.탈퇴 2.취소 : "));
+		if(result == 1) {
 			user.setId(saved.getId());
 			((UserMapper)Session.getMapper("userMapper")).deleteMyMusicId(user);
 			((UserMapper)Session.getMapper("userMapper")).deleteUser(user);
 			System.out.println("회원탈퇴가 완료되었습니다.");
 			returnToMain();
+		}else {
+			System.out.println("탈퇴를 취소하였습니다.");
 		}
 	}
 
