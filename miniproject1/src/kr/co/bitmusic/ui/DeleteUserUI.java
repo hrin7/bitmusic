22a897bcb10c3fbfe8669495eaad699e2389a3a6
Sujin <sujinki6020@gmail.com>
2018-03-18 17:52:02 +0900

    deleteuserui 확인용

--- miniproject1/src/kr/co/bitmusic/ui/DeleteUserUI.java
+++ miniproject1/src/kr/co/bitmusic/ui/DeleteUserUI.java
@@ -10,10 +10,13 @@ public class DeleteUserUI extends BaseBitMusicUI {
 	
 	public void service() {
 		User user = new User();
-		user.setId(saved.getId());
-		((UserMapper)Session.getMapper("userMapper")).deleteUser(user);
-		System.out.println("회원탈퇴가 완료되었습니다.");
-		returnToMain();
+		user.setPassword(getStr("정말 탈퇴하시겠습니까? 비밀번호를 한 번 더 입력해 주세요 : "));
+		if(user.getPassword()==saved.getPassword()) {
+			user.setId(saved.getId());
+			((UserMapper)Session.getMapper("userMapper")).deleteUser(user);
+			System.out.println("회원탈퇴가 완료되었습니다.");
+			returnToMain();
+		}
 	}
 
 	public void returnToMain() {
