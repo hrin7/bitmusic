3aaaec859e4860276419e00cac14407c6bf512c8
Sujin <sujinki6020@gmail.com>
2018-03-19 10:44:35 +0900

    회원가입 오류처리 추가

--- miniproject1/src/kr/co/bitmusic/ui/JoinUserUI.java
+++ miniproject1/src/kr/co/bitmusic/ui/JoinUserUI.java
@@ -7,18 +7,23 @@ import kr.co.bitmusic.mapper.UserMapper;
 public class JoinUserUI extends BaseBitMusicUI {
 
 	public void service() {
-		User userVO = new User();
-		System.out.println();
-		userVO.setId(getStr("Id를 설정하세요 : "));
-		userVO.setPassword(getStr("비밀번호를 설정하세요 : "));
-		userVO.setPasswordHint(getStr("비밀번호 힌트를 입력하세요(ex.좋아하는 책, 좋아하는 영화 등) : "));
-		userVO.setName(getStr("이름을 입력하세요 : "));
-		userVO.setAge(getInt("나이를 입력하세요 : "));
-		userVO.setGender(getStr("성별을 입력하세요(남 or 여) : "));
-		userVO.setEmail(getStr("이메일을 입력하세요 : "));
-		((UserMapper)Session.getMapper("userMapper")).insertUser(userVO);
-		System.out.println();
-		System.out.println("회원가입이 완료되었습니다.");
+		try {
+			User userVO = new User();
+			System.out.println();
+			userVO.setId(getStr("Id를 설정하세요 : "));
+			userVO.setPassword(getStr("비밀번호를 설정하세요 : "));
+			userVO.setPasswordHint(getStr("비밀번호 힌트를 입력하세요(ex.좋아하는 책, 좋아하는 영화 등) : "));
+			userVO.setName(getStr("이름을 입력하세요 : "));
+			userVO.setAge(getInt("나이를 입력하세요 : "));
+			userVO.setGender(getStr("성별을 입력하세요(남 or 여) : "));
+			userVO.setEmail(getStr("이메일을 입력하세요 : "));
+			((UserMapper)Session.getMapper("userMapper")).insertUser(userVO);
+			System.out.println();
+			System.out.println("회원가입이 완료되었습니다.");
+		} catch (Exception e) {
+			System.out.println("이미 존재하는 아이디입니다.");
+			new JoinUserUI().service();
+		}
 	}
 
 }
